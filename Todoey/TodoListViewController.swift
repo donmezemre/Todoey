5ed7a96ad69169c3257dd4a5f3fe22c5e3c95fd9
Emre Dönmez <emredonmez@emre-macbook-pro.local>
2018-02-28 23:38:07 +0300

    Data persistent complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,9 +12,14 @@ class TodoListViewController: UITableViewController {
 
     
     var itemArray = ["Oyun oyna","Yemek ye","Uyu"]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
     }
     
@@ -61,7 +66,7 @@ class TodoListViewController: UITableViewController {
             
     
             self.itemArray.append(textField.text!)
-            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
